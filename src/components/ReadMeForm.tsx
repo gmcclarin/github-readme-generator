00aab0a37d3ed6a589532d2957931998812066ea
Grace Nieboer <grace.nieboer@outlook.com>
2025-06-07 17:19:22 -0400

    install material ui and start using

--- src/components/ReadMeForm.tsx
+++ src/components/ReadMeForm.tsx
@@ -1,4 +1,6 @@
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import generateMarkdown from "../utils/generateMarkdown";
+import { Button, TextField } from "@mui/material";
 
 type ReadMeFormValues = {
 
@@ -8,13 +10,20 @@ export default function ReadMeForm () {
     const {
         register,
         handleSubmit,
-        watch,
         formState
     } = useForm();
 
+    const onSubmit: SubmitHandler<ReadMeFormValues> = (data) => {
+        generateMarkdown(data);
+    }
+
 
     return (
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            
+            <Button
+            type="submit"
+            >Make my ReadMe!</Button>
         </form>
     )
 }
