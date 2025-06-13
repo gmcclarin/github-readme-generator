5d2695981523f6093da8bda9ebcd86c5c2b84ec1
Grace Nieboer <grace.nieboer@outlook.com>
2025-06-12 23:12:39 -0400

    skill setup

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,13 +1,21 @@
-import { Checkbox } from "@mui/material";
+import { Checkbox, FormControlLabel } from "@mui/material";
+import { type Skill } from "../types/skill";
 
 export default function Skills() {
-
-    return (
-        <>
-
-        </>
-    )
+  return (
+    <>
+      {skills.map((s) => (
+        <Skill {...s} />
+      ))}
+    </>
+  );
 }
 
+const Skill = (skill: Skill) => (
+  <FormControlLabel label={skill.name} control={<Checkbox />} />
+);
 
-const SkillList = []
+const skills: Skill[] = [
+    
+
+];
